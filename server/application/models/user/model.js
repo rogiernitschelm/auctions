1c09f59ccf557524c66aaf58035ca5f8af8c76a1
Rogier Nitschelm <rogier@invoicefinance.nl>
2017-03-29 20:00:49 +0200

    wip

--- server/application/models/user/model.js
+++ server/application/models/user/model.js
@@ -10,15 +10,23 @@ UserSchema.pre('save', function save(next) {
     return next();
   }
 
-  user.password = passwordEncryptor(user)
+  passwordEncryptor(user)
     .then(() => next());
 });
 
 UserSchema.pre('update', function (next) {
   const user = this._update.$set;
 
-  user.password = passwordEncryptor(user)
-    .then(() => next());
+  if (this._update.$set.password) {
+    console.warn(
+      `
+      WARNING: You are not supposed to use update-middleware for updating secure
+      information. Please use find() and then save().
+      `
+    );
+    passwordEncryptor(user)
+      .then(() => next());
+  }
 });
 
 UserSchema
